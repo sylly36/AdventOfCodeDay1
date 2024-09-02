38d86175a5f20b85c059c6a88cdf491a0f76513b
d294626 <117263785+d294626@users.noreply.github.com>
2024-09-02 09:24:30 +0200

    Start Commit

---
+++ AdventOFCodeDay1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOFCodeDay1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
